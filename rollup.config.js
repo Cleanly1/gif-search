0eacdc43931c3515d781d0075875414de723be6b
Cleanly <oskar.turesson99@gmail.com>
2020-01-21 18:03:57 +0100

    added scss

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,24 @@
 import browsersync from "rollup-plugin-browsersync";
 import { terser } from "rollup-plugin-terser";
+import postcss from "rollup-plugin-postcss";
+import postcssnormalize from "postcss-normalize";
+import autoprefixer from "autoprefixer";
 
 const isProduction = process.env.NODE_ENV === "production";
 const isDevelopment = isProduction === false;
 
-if (isDevelopment) {
-  module.exports = {
-    input: "src/scripts/index.js",
-    output: {
-      file: "public/giphy.js",
-      format: "iife"
-    },
-    plugins: [browsersync({ server: "public" })]
-  };
-} else {
-  module.exports = {
-    input: "src/scripts/index.js",
-    output: [
-      {
-        file: "public/giphy.js",
-        format: "iife"
-      },
-      {
-        file: "public/giphy.min.js",
-        format: "iife",
-        name: "version",
-        plugins: [terser()]
-      }
-    ]
-  };
-}
+module.exports = {
+  input: "src/scripts/index.js",
+  output: {
+    file: "public/giphy.js",
+    format: "iife"
+  },
+  plugins: [
+    isDevelopment && browsersync({ server: "public" }),
+    postcss({
+      extract: true,
+      plugins: [postcssnormalize(), autoprefixer()]
+    }),
+    isProduction && terser()
+  ]
+};
